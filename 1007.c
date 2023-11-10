43edfca83823df6559bbe9214403a75c530aae52
MD. MOINUL ISLAM <118617222+MOINUL909@users.noreply.github.com>
2023-11-10 11:27:14 +0600

    Add files via upload

---
+++ 1007.c
@@ -0,0 +1,17 @@
+/*MOINUL*/
+#include <stdio.h>
+
+int main() {
+    int A,B,C,D,DIFERENCA;
+    scanf("%d%d%d%d",&A,&B,&C,&D);
+    DIFERENCA=(A*B-C*D);
+    printf("DIFERENCA = %d\n",DIFERENCA);
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
